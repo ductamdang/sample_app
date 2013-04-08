2a80dfc7aac40bfde9497b9e33a7b4b3c912c632
TamDD-PC <tamdd-pc@tamddpc-H61M-DS2.(none)>
2013-04-08 15:13:31 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '58980edc52d454bfe22f99480a7dd089cab2fa1d73440779362dff5f0e9e6d53bb9e1a5ee6f27c205954d4ab7ce667e891d7d210b8bbc73574081b5a5176d55a'
