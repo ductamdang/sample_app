d994360311dddb5ce67cb5d32bbae6126fac5667
TamDD-PC <tamdd-pc@tamddpc-H61M-DS2.(none)>
2013-04-11 13:47:02 +0900

    Update users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     @current_user  ||= User.find_by_remember_token(cookies[:remember_token])  # Useless! Don't use this line.
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
